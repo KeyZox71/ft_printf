fe1555a4ebd3188e701039f7d734d89651964d2c
Adam <adjoly@student.42angouleme.fr>
2023-11-17 19:11:35 +0100

    starting...

---
+++ libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adjoly <adjoly@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/17 16:50:36 by adjoly            #+#    #+#             */
+/*   Updated: 2023/11/17 16:57:31 by adjoly           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <stdlib.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *format, ...);
+
+#endif
